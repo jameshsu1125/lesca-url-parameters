ed328ffe233762d6e1cf987a9a8a25eeadc97123
jameshsu1125 <70932507+jameshsu1125@users.noreply.github.com>
2022-01-27 16:05:42 +0800

    upgrade

--- src/docs/index.js
+++ src/docs/index.js
@@ -1,56 +1,39 @@
 import React from 'react';
 import { render } from 'react-dom';
-import QueryString from '../lib/index';
+import { Code, Navation } from './components';
+import Demo from './demo';
 import './styles.less';
 
-function Page() {
+const homepage = 'https://github.com/jameshsu1125/lesca-url-parameters';
+const name = 'lesca-url-parameters';
+const description = 'get query string parameters value';
+
+const Page = () => {
 	return (
 		<>
-			<div>
-				<a href='./?name=James&age=40&birthday=1981'>click to test this page</a>
-			</div>
-			<div>
-				<h1>install</h1>
-				<p>npm install lesca-url-parameters --save</p>
-			</div>
-			<div>
-				<code>
-					QueryString.get('name') <br />
-					<span>// {JSON.stringify(QueryString.get('name'))}</span>
-				</code>
-			</div>
-			<div>
-				<code>
-					QueryString.get('age') <br />
-					<span>// {JSON.stringify(QueryString.get('age'))}</span>
-				</code>
-			</div>
-			<div>
-				<code>
-					QueryString.get('birthday') <br />
-					<span>// {JSON.stringify(QueryString.get('birthday'))}</span>
-				</code>
-			</div>
-			<div>
-				<code>
-					QueryString.remove('birthday') <br />
-					<span>// {JSON.stringify(QueryString.remove('birthday'))}</span>
-				</code>
-			</div>
-			<div>
-				<code>
-					QueryString.root() <br />
-					<span>// {JSON.stringify(QueryString.root())}</span>
-				</code>
-			</div>
-			<div>
-				<code>
-					QueryString.file() <br />
-					<span>// {JSON.stringify(QueryString.file())}</span>
-				</code>
+			<Navation />
+			<div className='content'>
+				<div>
+					<h1>{name}</h1>
+					<figcaption>{description}</figcaption>
+				</div>
+				<div>
+					<h2>install</h2>
+					<Code code={`npm install ${name} --save`} theme='command' />
+				</div>
+				<div>
+					<a href='./?name=James&age=40&birthday=1981'>click to test this page</a>
+				</div>
+				<div>
+					<Demo />
+				</div>
+				<div>
+					<h2>Usage</h2>
+					<a href={homepage}>Documentation</a>
+				</div>
 			</div>
 		</>
 	);
-}
+};
 
 render(<Page />, document.getElementById('app'));
